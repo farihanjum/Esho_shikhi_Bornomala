14da64c90cb8210a096290130fbcfc1615c898e3
farihanjum <farihaanjum.24csedu.022@gmail.com>
2019-10-26 19:13:44 +0600

    Initial commit

---
+++ app/src/main/java/com/example/esho_shikhi_bornomala/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.esho_shikhi_bornomala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
